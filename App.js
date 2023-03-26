cc6cc7b87b9f8338b7939c8d0407a796a5964674
prajwal zingare <prajwalzingre@gmail.com>
2023-03-26 12:43:08 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading1 = React.createElement("h1", { id: "heading1" }, "heading1");
+const heading2 = React.createElement("h2", { id: "heading2" }, "heading2");
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
